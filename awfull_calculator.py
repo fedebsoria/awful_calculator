679fa6a84787de19ed843272b9ba2f79c805d292
F <fsoria7@outlook.com>
2023-08-13 13:32:03 +0200

    fix close bug

--- awfull_calculator.py
+++ awfull_calculator.py
@@ -6,25 +6,24 @@ def calculator():
     print("Welcome to the world's worst calculator: only acepts 2 numbers:\n")
     print("Write CLOSE to close.")
     n1 = input("Insert number:  ")
+    if n1 == "c" or n1 == "CLOSE" or "close":
+      exit()
     oper = input("Insert operation symbol:  ")
     n2 = input("Insert number:  ")
-    total = 0
-    if n1 == "c" or n1 == "CLOSE" or "close":
-      sys.exit()
-    else:  
-      if oper == "+":
-        total = int(n1)+int(n2)
-      elif oper == "-":
-        total = int(n1)-int(n2)
-      elif oper == "*":
-        total = int(n1)*int(n2)
-      elif oper == "/" and n2 == "0":
-        print("You can't divide by 0, you silly.")
-      elif oper == "/":
-        total = int(n1)/int(n2)
-      else:
-        print("Something is wrong...")
-      return print(total)
+    total = 0  
+    if oper == "+":
+      total = int(n1)+int(n2)
+    elif oper == "-":
+      total = int(n1)-int(n2)
+    elif oper == "*":
+      total = int(n1)*int(n2)
+    elif oper == "/" and n2 == "0":
+      print("You can't divide by 0, you silly.")
+    elif oper == "/":
+      total = int(n1)/int(n2)
+    else:
+      print("Something is wrong...")
+    return print(total)
     
 def main():
   #call the function and make it in a loop
